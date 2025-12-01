f90bf95d7721ac37022c9a6043b1210e65fb247b
alexandremoshage <61665435+alexandremoshage@users.noreply.github.com>
2025-12-01 02:06:46 -0400

    Adjust heads-up action order

--- WindowsFormsApp2/UnitTestProject1/testesJogadas.cs
+++ WindowsFormsApp2/UnitTestProject1/testesJogadas.cs
@@ -133,5 +133,113 @@ namespace UnitTestProject1
 
             CollectionAssert.AreEqual(new List<string> { "big", "botao", "small" }, ordemAcoes);
         }
+
+        [TestMethod]
+        public void ExecutarAcoesJogadores_HeadsUp_PreFlop_DeveComecarNoBigBlind()
+        {
+            var ordemAcoes = new List<string>();
+
+            var jogadorSmall = new Mock<IJogador>();
+            jogadorSmall.SetupAllProperties();
+            jogadorSmall.Object.Posicao = EPosicao.small;
+            jogadorSmall.Object.IsNaMao = true;
+            jogadorSmall.Object.Fichas = 10;
+            jogadorSmall.Setup(j => j.AcaoJogador()).Returns(() =>
+            {
+                ordemAcoes.Add("small");
+                return (EAcao.check, 0);
+            });
+
+            var jogadorBig = new Mock<IJogador>();
+            jogadorBig.SetupAllProperties();
+            jogadorBig.Object.Posicao = EPosicao.big;
+            jogadorBig.Object.IsNaMao = true;
+            jogadorBig.Object.Fichas = 10;
+            jogadorBig.Setup(j => j.AcaoJogador()).Returns(() =>
+            {
+                ordemAcoes.Add("big");
+                return (EAcao.check, 0);
+            });
+
+            var mesa = new Mesa();
+            mesa.jogadores = new List<IJogador> { jogadorSmall.Object, jogadorBig.Object };
+
+            mesa.ExecutarAcoesJogadores(ERodada.PreFlop);
+
+            CollectionAssert.AreEqual(new List<string> { "big", "small" }, ordemAcoes);
+        }
+
+        [TestMethod]
+        public void ExecutarAcoesJogadores_HeadsUp_PosFlop_DeveComecarNoSmallBlind()
+        {
+            var ordemAcoes = new List<string>();
+
+            var jogadorSmall = new Mock<IJogador>();
+            jogadorSmall.SetupAllProperties();
+            jogadorSmall.Object.Posicao = EPosicao.small;
+            jogadorSmall.Object.IsNaMao = true;
+            jogadorSmall.Object.Fichas = 10;
+            jogadorSmall.Setup(j => j.AcaoJogador()).Returns(() =>
+            {
+                ordemAcoes.Add("small");
+                return (EAcao.check, 0);
+            });
+
+            var jogadorBig = new Mock<IJogador>();
+            jogadorBig.SetupAllProperties();
+            jogadorBig.Object.Posicao = EPosicao.big;
+            jogadorBig.Object.IsNaMao = true;
+            jogadorBig.Object.Fichas = 10;
+            jogadorBig.Setup(j => j.AcaoJogador()).Returns(() =>
+            {
+                ordemAcoes.Add("big");
+                return (EAcao.check, 0);
+            });
+
+            var mesa = new Mesa();
+            mesa.jogadores = new List<IJogador> { jogadorSmall.Object, jogadorBig.Object };
+
+            mesa.ExecutarAcoesJogadores(ERodada.Flop);
+
+            CollectionAssert.AreEqual(new List<string> { "small", "big" }, ordemAcoes);
+        }
+
+        [TestMethod]
+        public void ExecutarAcoesJogadores_DeveDistribuirPoteAoFinalDaRodada()
+        {
+            var mesa = new Mesa();
+            mesa.jogadores = new List<IJogador>();
+
+            var jogadorSmall = new Jogador(
+                new List<Carta> { new Carta(ECarta.As, ENipe.Copas), new Carta(ECarta.Rei, ENipe.Copas) },
+                mesa.jogadores.Count + 1)
+            { Posicao = EPosicao.small };
+
+            var jogadorBig = new Jogador(
+                new List<Carta> { new Carta(ECarta.Dez, ENipe.Espadas), new Carta(ECarta.Oito, ENipe.Paus) },
+                mesa.jogadores.Count + 1)
+            { Posicao = EPosicao.big };
+
+            mesa.jogadores.Add(jogadorSmall);
+            mesa.jogadores.Add(jogadorBig);
+
+            mesa.bordo = new List<Carta>
+            {
+                new Carta(ECarta.Dez, ENipe.Copas),
+                new Carta(ECarta.Oito, ENipe.Copas),
+                new Carta(ECarta.Dois, ENipe.Copas),
+                new Carta(ECarta.Tres, ENipe.Espadas),
+                new Carta(ECarta.Quatro, ENipe.Paus)
+            };
+
+            jogadorSmall.SetarAcaoJogador(EAcao.raise, 5);
+            jogadorBig.SetarAcaoJogador(EAcao.call, 5);
+
+            mesa.ExecutarAcoesJogadores(ERodada.PreFlop);
+
+            Assert.AreEqual(30, jogadorSmall.Fichas);
+            Assert.AreEqual(20, jogadorBig.Fichas);
+            Assert.AreEqual(0, mesa.pote);
+        }
     }
 }
